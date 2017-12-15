6588e74fe9387ed72b3eb3ec3e8b52c40b1a72d0
Julia Smith <juliasmith@Julias-MacBook-Air.local>
2017-12-15 06:35:02 -0800

    checkPrime function

---
+++ final_challenge.js
@@ -0,0 +1,11 @@
+function checkPrime(number) {
+    (i == 2; i <= (number) / 2; i++)
+        if ((number) % i == 0) {
+            return "not prime"
+        }
+    return "prime"
+}
+
+function factorTree(number){
+
+}
