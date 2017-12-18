9293c48715e559777c203f6f6bd7f9064c77f797
Julia Smith <juliasmith@Julias-MacBook-Air.local>
2017-12-17 17:04:47 -0800

    checkPrime function

--- final_challenge.js
+++ final_challenge.js
@@ -1,5 +1,7 @@
 function checkPrime(number) {
-    if ((number) <= 1 || (number) )
+    if ((number) <= 1 || (number) != Math.floor(number)) {
+        return "not prime"
+    }
     for (i = 2; i <= (number) / 2; i++) {
         if ((number) % i == 0) {
             return "not prime";
@@ -8,14 +10,26 @@ function checkPrime(number) {
     return "prime";
 }
 
-function timesTable(number){
-document.write ("<table border = 1>");
-for (i = 1 ; i < (number) + 1 ; i++){
-    document.write("<tr>");
-    for (j = 1 ; j < (number) + 1; j++) {
-        document.write ("<td>" , i*j , "</td>");
+function timesTable(number) {
+    document.write("<table border = 1");
+    for (i = 1; i < number + 1; i++) {
+        document.write("<tr>");
+        for (j = 1; j < number + 1; j++) {
+            document.write("<td>", i * j, "</td>");
+        }
+        document.write("</tr>");
     }
-    document.write ("</tr>");
+    document.write("</table>");
 }
-document.write ("</table>");
+
+function primeTable(number) {
+    document.write("<table border = 1");
+    for (i = 1; i < number + 1; i++) {
+        document.write("<tr>");
+        for (j = 1; j < number + 1; j++) {
+            document.write("<td>", i * j, "</td>");
+        }
+        document.write("</tr>");
+    }
+    document.write("</table>");
 }
